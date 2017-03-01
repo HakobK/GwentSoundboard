d517349f663c7d276358512843f2b8bbbb7c65ac
Hakob Khachadurian <hakobk1@gmail.com>
2017-03-01 03:13:24 +0100

    allstuff

---
+++ app/src/main/java/com/hkn/gwentsoundboard/MenuActivity.java
@@ -0,0 +1,48 @@
+package com.hkn.gwentsoundboard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-9987402400398249~8273758814");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    public void navigate(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void navigateSettings(View view) {
+
+    }
+
+    public void sendMail(View view) {
+        String email = "gwentboarddev@gmail.com";
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { email });
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Gwentboard");
+        this.startActivity(Intent.createChooser(intent, ""));
+    }
+}
